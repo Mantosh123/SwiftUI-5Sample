4ca7b96f8f5bc6d1155be693f66ad1907cea5bda
Mantosh Kumar <kumar.mantosh83@gmail.com>
2024-11-14 20:28:23 +0530

    Add timer for game end and restart.

--- CardGame/CardGame/ContentView.swift
+++ CardGame/CardGame/ContentView.swift
@@ -17,6 +17,11 @@ extension View {
 struct ContentView: View {
     
     @State private var cards = Array<Card>(repeating: .example, count: 10)
+    @State private var  timeRemaning = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    @Environment(\.scenePhase) var scenePhase
+    @State private var isActive = true
     
     var body: some View {
         
@@ -25,6 +30,12 @@ struct ContentView: View {
                 .resizable()
                 .ignoresSafeArea()
             VStack {
+                Text("Timer: \(timeRemaning)")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .padding()
+                    .background(.black)
+                    .clipShape(.capsule)
                 ZStack {
                     ForEach(0..<cards.count, id: \.self) { index in
                         CardView(card: cards[index]) {
@@ -32,8 +43,33 @@ struct ContentView: View {
                                 removeCard(at: index)
                             }
                         }
-                            .stacked(at: index, in: cards.count)
+                        .stacked(at: index, in: cards.count)
                     }
+                }
+                .allowsHitTesting(timeRemaning > 0)
+                
+                if cards.isEmpty {
+                    Button("Start Again", action: resetCard)
+                        .padding()
+                        .background(.white)
+                        .foregroundStyle(.black)
+                        .clipShape(.capsule)
+                }
+            }
+            .onReceive(timer) { timer in
+                guard isActive else { return }
+                
+                if timeRemaning > 0 {
+                    timeRemaning -= 1
+                }
+            }
+            .onChange(of: scenePhase) {
+                if scenePhase == .active {
+                    if cards.isEmpty {
+                        isActive = true
+                    }
+                } else {
+                    isActive = false
                 }
             }
         }
@@ -42,6 +78,16 @@ struct ContentView: View {
     
     func removeCard(at index: Int) {
         cards.remove(at: index)
+        
+        if cards.isEmpty {
+            isActive = false
+        }
+    }
+    
+    func resetCard() {
+        cards = Array<Card>(repeating: .example, count: 10)
+        timeRemaning = 100
+        isActive = true
     }
     
 }
