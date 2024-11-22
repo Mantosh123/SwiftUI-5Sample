cc1e3b950578e43441237239def116ced75ff8c0
Mantosh Kumar <kumar.mantosh83@gmail.com>
2024-11-22 21:01:54 +0530

    Start MyResort app

---
+++ MyResort/MyResort/MyResortApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyResortApp.swift
+//  MyResort
+//
+//  Created by Mantosh Kumar on 22/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MyResortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyResort/MyResortTests/MyResortTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyResortTests.swift
+//  MyResortTests
+//
+//  Created by Mantosh Kumar on 22/11/24.
+//
+
+import Testing
+@testable import MyResort
+
+struct MyResortTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
